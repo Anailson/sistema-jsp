691785d7a3c0788b82f0be22c0721034cfa06e98
anailson ribeiro <arsantos315@gmail.com>
2022-05-21 20:26:31 -0300

    Projeto-Criado

---
+++ src/main/java/filter/FilterAutenticacao.java
@@ -0,0 +1,62 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionActivationListener;
+
+/**
+ * FILTER INTERCEPTA TODAS AS REQUSIÇÕES QUE VIER DO PROJETO OU MAPEAMENTO
+ */
+@WebFilter(urlPatterns = {"/principal/*"})
+public class FilterAutenticacao implements Filter {
+
+    
+    public FilterAutenticacao() {
+      
+    }
+
+	//encerra os processos quando o servidor é parado - exemplo:matar os processos de conexão com o banco
+	public void destroy() {
+		
+	}
+
+	//intercepta as requisições e as respostas do sistema. Exemplo: validação de autenticação etc.
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		String usuarioLogado = (String) session.getAttribute("usuario");
+		
+		String urlParaAutenticar = req.getServletPath();//URL que está sendo acessada.
+		
+		//VALIDAR SER ESTÁ LOGADO - Caso não esteja redirecionar para a tela de login
+		if (usuarioLogado == null || (usuarioLogado != null && usuarioLogado.isEmpty()) &&
+				!urlParaAutenticar.contains("/principal/ServletLogin")) {
+			
+			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+			request.setAttribute("msg", "Por favor realize o login!");
+			redireciona.forward(request, response);
+			return; //para a execução e redireciona para o login
+			
+		} else {
+			chain.doFilter(request, response);
+		}
+				
+	}
+
+	//iniciar os processo ou recursos qd o servidor sobre o projeto - exemplo: iniciar a conexão com o banco
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
---
+++ src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,76 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.tagplugins.jstl.core.If;
+
+import model.ModelLogin;
+
+//É O CHAMADO CONTROLLER SÃO AS servlets ou ServletLoginController
+@WebServlet("/ServletLogin") // MAPEAMENTO DA URL QUE VEM DA TELA
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletLogin() {
+
+	}
+
+	// RECEBER OS DADOS PELA URL EM PARAMETROS
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/// RECEBER OS DADOS ENVIADOS POR UM FORMULÁRIO
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		String url   = request.getParameter("url");
+
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			//simulando acesso
+			if (modelLogin.getLogin().equalsIgnoreCase("admin")
+					&& modelLogin.getSenha().equalsIgnoreCase("admin")){//SIMULANDO LOGIN
+				
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				
+				if(url == null || url.equals("null")) {
+					url = "principal/principal.jsp";
+					
+				}
+				
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+				redirecionar.forward(request, response);
+			
+				
+			}else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!!");
+				redirecionar.forward(request, response);
+			}//teste
+			
+			
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe o login e senha corretamente!!");
+			redirecionar.forward(request, response);
+		}
+
+	}
+
+}
