2966819a685acb70ba2851d6f3cc11c665cbef4b
anailson ribeiro <arsantos315@gmail.com>
2022-10-23 12:12:05 -0300

    criando DAOLogin e método de autenticação no BD

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -11,12 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jasper.tagplugins.jstl.core.If;
 
+import dao.DAOLoginRepository;
 import model.ModelLogin;
 
 //É O CHAMADO CONTROLLER SÃO AS servlets ou ServletLoginController
 @WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) // MAPEAMENTO DA URL QUE VEM DA TELA
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
 	public ServletLogin() {
 
@@ -37,15 +40,16 @@ public class ServletLogin extends HttpServlet {
 		String senha = request.getParameter("senha");
 		String url   = request.getParameter("url");
 
+		try {
+	
 		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
 
 			ModelLogin modelLogin = new ModelLogin();
 			modelLogin.setLogin(login);
 			modelLogin.setSenha(senha);
 			
-			//simulando acesso
-			if (modelLogin.getLogin().equalsIgnoreCase("admin")
-					&& modelLogin.getSenha().equalsIgnoreCase("admin")){//SIMULANDO LOGIN
+		
+			if (daoLoginRepository.validarAutenticacao(modelLogin)){//SIMULANDO LOGIN
 				
 				request.getSession().setAttribute("usuario", modelLogin.getLogin());
 				
@@ -69,6 +73,9 @@ public class ServletLogin extends HttpServlet {
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 			request.setAttribute("msg", "Informe o login e senha corretamente!!");
 			redirecionar.forward(request, response);
+		}
+	} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
